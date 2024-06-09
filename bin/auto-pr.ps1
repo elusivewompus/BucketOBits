3593fd1cd3cab7bb24cd729671a122ab9b9b281f
elusivewompus <hendy643@hotmail.com>
2024-06-09 02:05:19 +0100

    settings for actions

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "<username>/<bucketname>:main"
+    [String]$upstream = "elusivewompus/BucketOBits:master"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
